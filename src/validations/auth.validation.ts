7910d3aab7ada9f8fe54d2aca113ef2203b42584
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-10-26 02:35:14 +0100

    Update register validationd for middle name

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,8 +1,9 @@
-import Joi from "joi";
+import Joi, { string } from "joi";
 import { allowedCategories } from "./product.validation";
 
 export const registerValidationSchema = Joi.object({
     first_name: Joi.string().alphanum().required(),
+    middle_name: Joi.string().alphanum(),
     last_name: Joi.string().alphanum().required(),
     dob: Joi.date().required(),
     phone_numbers: Joi.array().min(1).unique(),
