16278506aa9a9968097752d57c9c509faf04b143
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-11-04 14:26:26 +0100

    Fix: fix cloudinary config issues

--- src/middlewares/upload.middlewares.ts
+++ src/middlewares/upload.middlewares.ts
@@ -4,9 +4,15 @@ import multer from "multer";
 import path from "path";
 import sharp from "sharp";
 import { v4 as uuidv4 } from "uuid";
-import Cloudinary from "../config/cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import { UploadApiResponse } from "cloudinary";
 
+// Cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 const MAX_FILE_SIZE = 1024 * 1024 * 10;
 const MAX_WIDTH = 2000;
@@ -55,7 +61,7 @@ const processCloudinaryImage = async (file: Express.Multer.File): Promise<string
 
         // Upload to Cloudinary
     return new Promise((resolve, reject) => {
-        const uploadStream = Cloudinary.uploader.upload_stream(
+        const uploadStream = cloudinary.uploader.upload_stream(
             {
                 folder: 'oyeah/tests',
                 resource_type: 'auto',
