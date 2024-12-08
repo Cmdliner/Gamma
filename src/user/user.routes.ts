ff91835e89d800a2d53400e03a0ba5d229fe4501
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-12-09 00:58:41 +0100

    Finalized image upload

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -11,7 +11,7 @@ user.get("/referrals", UserController.getReferredUsers);
 user.get("/referral-code", UserController.getReferralToken);
 user.put("/bank-details", UserController.editBankAccountDetails);
 user.put("/info", UserController.updateInfo);
-user.put("/display-picture", upload.single("display_pic"), UserController.getUserInfo);
+user.put("/display-picture", upload.single("display_pic"), UserController.updateDisplayPicture);
 
 
 export default user;
